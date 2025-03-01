498ebceb20cbf37324f98b454b2d77fef8428087
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2025-03-01 10:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const byteSize = (str) => {
+  // write your code here
 	if(str.trim()==0) return 0;
 	const encoder = new TextEncoder();
 const encodedData = encoder.encode(str);
 const byteLength = encodedData.length;
-return byteLength;
+	return byteLength;
 };
      
 // Do not change the code below
  const str = prompt("Enter some string.");
 alert(byteSize(str)); 
-  // write your code here
-};
-
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
